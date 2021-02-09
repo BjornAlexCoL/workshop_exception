187e443bcb6c73ceb54076c31fb6b447ed99231e
BjornAlexCoL <75667280+BjornAlexCoL@users.noreply.github.com>
2021-02-09 17:43:44 +0100

    Initial commit

---
+++ src/main/java/se/lexicon/exceptions/workshop/data_access/NameService.java
@@ -0,0 +1,117 @@
+package se.lexicon.exceptions.workshop.data_access;
+
+import java.sql.SQLOutput;
+import java.util.List;
+import java.util.Random;
+import java.util.zip.DataFormatException;
+
+import se.lexicon.exceptions.workshop.domain.Gender;
+import se.lexicon.exceptions.workshop.domain.Person;
+import se.lexicon.exceptions.workshop.fileIO.CSVReader_Writer;
+
+public class NameService {
+	
+
+	    private List<String>maleFirstNames;
+	    private List<String>femaleFirstNames;
+	    private List<String>lastNames;
+	    private static Random random = new Random();
+
+	    //should be no nulls
+	    public NameService(List<String> maleFirstNames, List<String> femaleFirstNames, List<String> lastNames) {
+
+	        this.maleFirstNames = maleFirstNames;
+	        this.femaleFirstNames = femaleFirstNames;
+	        this.lastNames = lastNames;
+	    }
+
+	    public Person getNewRandomPerson(){
+	        Gender gender = getRandomGender();
+	        Person person = null;
+	        switch (gender){
+	            case MALE:
+	                person = new Person(getRandomMaleFirstName(),getRandomLastName(),gender);
+	                break;
+	            case FEMALE:
+	                person = new Person(getRandomFemaleFirstName(),getRandomLastName(),gender);
+	                break;
+	        }
+	        return person;
+	    }
+
+
+	    public String getRandomFemaleFirstName(){
+	        return femaleFirstNames.get(random.nextInt(femaleFirstNames.size()));
+	    }
+
+	    public String getRandomMaleFirstName(){
+	        return maleFirstNames.get(random.nextInt(maleFirstNames.size()));
+	    }
+
+	    public String getRandomLastName(){
+	        return lastNames.get(random.nextInt(lastNames.size()));
+	    }
+
+	    public Gender getRandomGender(){
+	        return random.nextInt(100) > 50 ? Gender.FEMALE : Gender.MALE;
+	    }
+
+
+	    /**
+	     * Here you need to check if List<String> femaleFirstNames already contains the name
+	     * If name already exists throw a new custom exception you will have to create called
+	     * DuplicateNameException.
+	     * @param name
+	     */
+	    public void addFemaleFirstName(String name) throws DuplicateNameException {
+	    	if (femaleFirstNames.stream().filter(s->s.toString().equalsIgnoreCase(name))!=null)
+	    		throw new DuplicateNameException(name);
+	    	femaleFirstNames.add(name);
+	    	CSVReader_Writer.saveFemaleNames(femaleFirstNames);
+	    		
+	    }
+
+	    /**
+	     * Here you need to check if List<String> maleFirstNames already contains the name
+	     * If name already exists throw a new custom exception you will have to create called
+	     * DuplicateNameException.
+	     * @param name
+	     */
+	    public void addMaleFirstName(String name)throws DuplicateNameException {
+			if (maleFirstNames.stream().filter(s->s.toString().equalsIgnoreCase(name))!=null)
+				throw new DuplicateNameException(name);
+	    	maleFirstNames.add(name);
+	        CSVReader_Writer.saveMaleNames(maleFirstNames);
+	    }
+
+	    /**
+	     * Here you need to check if List<String> lastNames already contains the name
+	     * If name already exists throw a new custom exception you will have to create called
+	     * DuplicateNameException.
+	     * @param lastName
+	     */
+	    public void addLastName(String lastName) throws DuplicateNameException {
+
+	    	if (lastNames.stream().filter(s ->s.toString().equalsIgnoreCase(lastName))!=null)
+	    		throw new DuplicateNameException(lastName);
+
+	    	lastNames.add(lastName);
+	        CSVReader_Writer.saveLastNames(lastNames);
+	    }
+
+
+	
+
+
+	private class DuplicateNameException extends Exception {
+	    	String name;
+			public DuplicateNameException(String name) {
+				this.name = name;
+				System.out.println(name + " exists try another.");
+			}
+		public String getName() {
+			return name;
+		}
+	}
+}
+
